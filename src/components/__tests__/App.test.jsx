f584432b9dca74ff2d1944c02807cf1fd61c9f10
jonsoku2 <whdtjr2792@gmail.com>
2020-02-14 08:27:38 +0900

    테스트파일 리팩토링

--- src/components/__tests__/App.test.jsx
+++ src/components/__tests__/App.test.jsx
@@ -4,12 +4,17 @@ import App from '../App'
 import CommentBox from '../CommentBox'
 import CommentList from '../CommentList'
 
+let wrapped // 변수 스코프에 걸리니 전역변수로 만든다.
+
+// 이 파일의 첫번째 테스트 위에서 각 함수가 끝나기 전에 함수를 호출한다.
+beforeEach(() => {
+  wrapped = shallow(<App />)
+})
+
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />)
   expect(wrapped.find(CommentBox).length).toEqual(1)
 })
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />)
   expect(wrapped.find(CommentList).length).toEqual(1)
 })
