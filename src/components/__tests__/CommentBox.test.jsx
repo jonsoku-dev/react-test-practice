1142dd5b162c40e0fdba23253984111a29ec007f
jonsoku2 <whdtjr2792@gmail.com>
2020-02-14 08:36:30 +0900

    comment box 컴포넌트 생성 및 테스트파일 생성(미작성)

---
+++ src/components/__tests__/CommentBox.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import CommentBox from 'component/CommentBox'
+
+// 아래 명령을 실행하여 length를 출력 할 수 있다.
+// console.log(wrapped.find('textarea').length)
+it('has a text area and a button', () => {})
