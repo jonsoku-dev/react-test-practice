327832a0159c019a0de99b4b494865e024042929
jonsoku2 <whdtjr2792@gmail.com>
2020-02-14 08:40:56 +0900

    CommentBox test 코드 작성

--- src/components/__tests__/CommentBox.test.jsx
+++ src/components/__tests__/CommentBox.test.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
-import CommentBox from 'component/CommentBox'
+import { mount } from 'enzyme'
+import CommentBox from '../CommentBox'
 
 // 아래 명령을 실행하여 length를 출력 할 수 있다.
 // console.log(wrapped.find('textarea').length)
-it('has a text area and a button', () => {})
+it('has a text area and a button', () => {
+  const wrapped = mount(<CommentBox />)
+
+  //   console.log(wrapped.find('textarea').length)
+  //   console.log(wrapped.find('button').length)
+
+  expect(wrapped.find('textarea').length).toEqual(1)
+  expect(wrapped.find('button').length).toEqual(1)
+})
