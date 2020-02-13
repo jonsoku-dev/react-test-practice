5a2f8ca3804b973790fc74a5a81ca7616205a950
jonsoku2 <whdtjr2792@gmail.com>
2020-02-14 08:43:19 +0900

    beforeEach -> it -> afterEach

--- src/components/__tests__/CommentBox.test.jsx
+++ src/components/__tests__/CommentBox.test.jsx
@@ -4,12 +4,24 @@ import CommentBox from '../CommentBox'
 
 // 아래 명령을 실행하여 length를 출력 할 수 있다.
 // console.log(wrapped.find('textarea').length)
-it('has a text area and a button', () => {
-  const wrapped = mount(<CommentBox />)
 
+let wrapped
+
+// 1
+beforeEach(() => {
+  wrapped = mount(<CommentBox />)
+})
+
+// 3
+
+afterEach(() => {
+  wrapped.unmount()
+})
+
+// 2
+it('has a text area and a button', () => {
   //   console.log(wrapped.find('textarea').length)
   //   console.log(wrapped.find('button').length)
-
   expect(wrapped.find('textarea').length).toEqual(1)
   expect(wrapped.find('button').length).toEqual(1)
 })
