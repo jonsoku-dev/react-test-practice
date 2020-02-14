1000a71519514a304eecce08f0c0361aea1b66c0
jonsoku2 <whdtjr2792@gmail.com>
2020-02-14 09:22:46 +0900

    react-redux 연결 (comment box component)

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 class CommentBox extends Component {
   state = { comment: '' }
@@ -11,6 +13,7 @@ class CommentBox extends Component {
     event.preventDefault()
 
     // TODO - Call an action creator
+    this.props.saveComment(this.state.comment)
     // And save the comment
     this.setState({ comment: '' })
   }
@@ -28,4 +31,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox
+export default connect(null, actions)(CommentBox)
