2f1dfd00b806cefc5bd08787f3ea324740063f91
rakutenjongseok <whdtjr2792@gmail.com>
2020-02-14 23:16:55 +0900

    test: axios로 fetch한 데이터 개수 테스트 (실패) - 2

--- src/__test__/integrations.test.jsx
+++ src/__test__/integrations.test.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Root from '../Root';
 import App from '../components/App';
+import moxios from 'moxios';
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('https://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetched #1' }, { name: 'Fetched #2' }],
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it('can fetch a list of comments and display them', () => {
   // Attempt to render the *entire* app
