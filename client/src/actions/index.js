384639038ca75bcd25ac7f89bc6a7dc806f13f21
jonsoku2 <whdtjr2792@gmail.com>
2020-02-18 04:32:43 +0900

    feat: redux-thunk 사용하기

---
+++ client/src/actions/index.js
@@ -0,0 +1,5 @@
+import { AUTH_USER } from "./type";
+
+export const signup = ({ email, password }) => dispatch => {
+  dispatch({ type: AUTH_USER, payload: "sdfjsdlfjlsd" });
+};
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,8 +1,16 @@
+import { AUTH_USER } from "../actions/type.js";
+
 const INITIAL_STATE = {
   authenticated: "", // String
   errorMessage: "" // String
 };
 
 export default (state = INITIAL_STATE, action) => {
-  return state;
+  const { type, payload } = action;
+  switch (type) {
+    case AUTH_USER:
+      return payload;
+    default:
+      return state;
+  }
 };
