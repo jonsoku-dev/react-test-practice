ade3de5c85e30037b994fe5792a9a4004479888d
jonsoku2 <whdtjr2792@gmail.com>
2020-02-18 04:13:46 +0900

    feat: router생성 , App HOC생성

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./Header";
 
-const App = () => {
-  return <div>App</div>;
+const App = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App>
+      <Route path="/" component={Welcome} />
+    </App>
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
