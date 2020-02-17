1fa3ba21aad03f6a089bee70f08afbfc0061a62b
jonsoku2 <whdtjr2792@gmail.com>
2020-02-17 19:58:13 +0900

    feat: passport strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,3 +2,26 @@ const passport = require("passport");
 const User = require("../models/User");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+// Setup options for JWT Strategy
+const jwtOptions = {};
+
+// Create JWT strategy
+// payload는 디코딩된 jwt 토큰이다.
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if the user ID in the payload exists in our database
+  // If it does, call 'done' with other
+  // otherwise, call done without a user object
+  User.findById(payload.subdomains, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Tell passport to use this strategy
