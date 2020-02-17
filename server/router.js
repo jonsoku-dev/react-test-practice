a03c40f53c79e673f24504de48926ce29a3e19f5
jonsoku2 <whdtjr2792@gmail.com>
2020-02-17 19:28:04 +0900

    feat: 회원가입 컨트롤러 - 1

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const auth = require("./controllers/auth");
+
 module.exports = function(app) {
-  app.get("/", function(req, res, next) {
-    res.send(["waterbottole", "phone", "paper"]);
-  });
+  app.post("/signup", auth.signup);
 };
