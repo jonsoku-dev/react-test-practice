5884c4b88bb359a1546e1638cb6c3db698a7600c
jonsoku2 <whdtjr2792@gmail.com>
2020-02-18 04:28:19 +0900

    feat: middleware 설정 ! - redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,21 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 // Redux
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
+import reduxThunk from "redux-thunk";
 
 // Components
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
 
+const middlewares = [reduxThunk];
+
+const store = createStore(reducers, {}, applyMiddleware(...middlewares));
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Provider store={store}>
     <BrowserRouter>
       <App>
         <Route path="/" component={Welcome} />
