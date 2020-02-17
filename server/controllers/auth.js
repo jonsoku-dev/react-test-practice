8d0b049fb37f86a0c6256992404565f69167ae59
jonsoku2 <whdtjr2792@gmail.com>
2020-02-17 19:35:03 +0900

    feat: 회원가입 컨트롤러 - 3

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,10 +6,23 @@ exports.signup = async (req, res, next) => {
     const password = req.body.password;
 
     // See if a user with the given email exists
-    const user = await User.findOne({ email: email });
+    const existingUser = await User.findOne({ email: email });
+
     // If a user with email does exist, return an error
+    if (existingUser) {
+      res.status(422).send({ error: "해당 이메일은 사용 중 입니다..." });
+    }
+
     // If a user with email does NOT exist, create and save user record
+    const newUser = new User({
+      email,
+      password
+    });
+
+    const user = await newUser.save();
+
     // Repond to request indicating the user was created
+    res.status(201).json({ success: true, data: user });
   } catch (err) {
     console.error(err);
   }
