583c4a1613a7b78f3675821ebc9c90d5cc41a35b
jonsoku2 <whdtjr2792@gmail.com>
2020-02-17 20:17:09 +0900

    feat: passport local 전략

--- server/router.js
+++ server/router.js
@@ -1,5 +1,12 @@
 const auth = require("./controllers/auth");
+const passportService = require("./services/passport");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
 
 module.exports = function(app) {
+  app.get("/", requireAuth, function(req, res) {
+    res.send({ hi: "there" });
+  });
   app.post("/signup", auth.signup);
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -2,9 +2,24 @@ const passport = require("passport");
 const User = require("../models/User");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+// Create local strategy
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function(
+  email,
+  password,
+  done
+) {
+  // 사용자이름과 비밀번호 호출이 올바른지 확인한다.
+  // 그렇지않으면 call done with false
+});
 
 // Setup options for JWT Strategy
-const jwtOptions = {};
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: process.env.JWT_SECRET
+};
 
 // Create JWT strategy
 // payload는 디코딩된 jwt 토큰이다.
@@ -12,7 +27,7 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
   // See if the user ID in the payload exists in our database
   // If it does, call 'done' with other
   // otherwise, call done without a user object
-  User.findById(payload.subdomains, function(err, user) {
+  User.findById(payload.id, function(err, user) {
     if (err) {
       return done(err, false);
     }
@@ -25,3 +40,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 // Tell passport to use this strategy
+passport.use(jwtLogin);
